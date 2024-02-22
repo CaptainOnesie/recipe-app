bd357e8cf6bb284dc5c439d3f8b5ac4fa07fbe33
CaptainOnesie <connor.sortome@gmail.com>
2024-02-21 22:35:33 -0800

    Tutorial continuation
    Continued with tutorial, updated route handlers and included logic for fakeish login handling.

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,12 +1,29 @@
+import { json } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random() * count);
+
+  const randomJoke = await db.joke.findMany({
+    orderBy: { updatedAt: "desc" },
+    skip: randomNumber,
+    take: 1,
+    select: { id: true, content: true, name: true }
+  });
+  return randomJoke.pop();
+}
+
 export default function JokesIndexRoute() {
-    return (
-      <div>
-        <p>Here's a random joke:</p>
-        <p>
-          I was wondering why the frisbee was getting bigger,
-          then it hit me.
-        </p>
-      </div>
-    );
-  }
-  
+  const data = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>
+        {data.content}
+      </p>
+      <Link to={data.id}>Permalink to {data.name}</Link>
+    </div>
+  );
+}
